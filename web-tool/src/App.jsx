805c79620ce9b4b945d786fbdf8aab1d908c64da
Kris Sankaran <sankaran.kris@gmail.com>
2020-04-21 19:38:13 -0400

    detecting key and click events

--- web-tool/src/App.jsx
+++ web-tool/src/App.jsx
@@ -10,16 +10,43 @@ class App extends Component {
     super(props);
     this.state = {basemap: "ESRI"};
     this.handleBasemapChange = this.handleBasemapChange.bind(this);
+    this.handleKeypress = this.handleKeypress.bind(this);
+    this.handleKeyup = this.handleKeyup.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount(){
+    document.addEventListener("keydown", this.handleKeypress, false);
+    document.addEventListener("keyup", this.handleKeyup, false);
   }
 
   handleBasemapChange(event) {
-    this.setState({basemap: event.target.value});
+    this.setState({
+      basemap: event.target.value,
+      keyPressed: false
+    });
+  }
+
+  handleKeypress(event) {
+    if (event.keyCode == 16) {
+      this.setState({keyPressed: true});
+    }
+  }
+
+  handleKeyup(event) {
+    this.setState({keyPressed: false});
+  }
+
+  handleClick(event) {
+    if (this.state.keyPressed) {
+      // do inference
+    }
   }
 
   render() {
     return (
       <div>
-        <div className="Map">
+        <div className="Map" onClick={this.handleClick}>
           <MapDisplay basemap={this.state.basemap}/>
         </div>
         <div className="Select">
